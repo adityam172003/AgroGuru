01d3221cd0f85ea71740818066a153d09fd38732
Aditya Mahajan <adityam172003@gmail.com>
2023-04-23 11:12:32 +0530

    adding userLoginAuthentication

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,7 +1,35 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import { MainNavbar } from '../Component/MainNavbar'
+import { useNavigate } from 'react-router-dom';
+import { useState,useEffect } from 'react';
+
+import axios from 'axios';
 function Main() {
+ 
+
+           const nevigate = useNavigate();
+           const [dat,setdat ] =useState()
+    const getuser = async()=>{
+        await axios.get('/user/getuser').
+        then((res)=>{
+           setdat(res.data.name);
+           console.log(res.data.name)
+        })
+        .catch(()=>{
+           
+            nevigate('/');
+            alert("Please Login first");
+        })
+        
+    
+           
+    }
+
+    useEffect(()=>{
+        getuser();
+    },[])
+
   return (
    <>
      
@@ -9,6 +37,8 @@ function Main() {
      <Outlet/>
    </>
   )
+
+
 }
 
 export default Main
