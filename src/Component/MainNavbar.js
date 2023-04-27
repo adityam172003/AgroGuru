c465bac362ef44866fed9e683ecf07a496f88f9a
Aditya Mahajan <adityam172003@gmail.com>
2023-04-27 09:46:32 +0530

    added logoutbutton

--- src/Component/MainNavbar.js
+++ src/Component/MainNavbar.js
@@ -1,7 +1,38 @@
+import axios from 'axios'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+
+
+
+
+
+
 
 export const MainNavbar = () => {
+
+  const nevigate = useNavigate();
+  
+const userLogout = async()=>{
+
+  axios.get('/user/logout')
+  .then(()=>{
+    nevigate('/');
+    alert("Logout");
+   
+    
+  })
+  .catch((err)=>{
+    console.log(err);
+  })
+}
+
   return (
-    <div>MainNavbar</div>
+    <div>MainNavbar
+
+    <button onClick={userLogout} >logout</button>
+
+    </div>
+  
   )
 }
