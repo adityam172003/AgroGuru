9783aec5cd9c9adf5009e58c27e504d34381b3b3
Rahul Patil <discoverrahul592@gmail.com>
2023-04-30 00:38:46 +0530

    added styling fir nursery form

--- src/Pages/Forms/NurseryForm.js
+++ src/Pages/Forms/NurseryForm.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import {yupResolver} from '@hookform/resolvers/yup'
 import * as yup from 'yup'
-import '../../Styles/Register.css'
+import '../../Styles/nursery_form_ui.css'
 import axios from 'axios'
 
 const NurseryForm = () => {
@@ -94,23 +94,72 @@ const NurseryForm = () => {
 
 
     return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-        <input type="text" placeholder='Enter Name of Nursery' {...register("name")}/>
-        <p>{errors.name?.message}</p>
-        <input type='text' placeholder='Address of Nursery' {...register('address')}/>
-        <p>{errors.address?.message}</p>
-        <input type="email" placeholder='Enter Contact-Email of Nursery' {...register("email")}/>
-        <p>{errors.email?.message}</p>
-        <input type="text" placeholder='Enter Contact-Number of Nursery' {...register("phone")}/>
-        <p>{errors.phone?.message}</p>
-        <input type='time' placeholder='Opening Time' {...register("openTime")}/>
-        <p>{errors.openTime?.message}</p>
-        <input type='time' placeholder='Closing Time' {...register("closeTime")}/>
-        <p>{errors.closeTime?.message}</p>
-        <input type="text" placeholder='Enter your Off-Day' {...register("offDay")}/>
-        <p>{errors.offDay?.message}</p>
-        <input type="submit"/>
-    </form>
+    // <form onSubmit={handleSubmit(onSubmit)}>
+    //     <input type="text" placeholder='Enter Name of Nursery' {...register("name")}/>
+    //     <p>{errors.name?.message}</p>
+    //     <input type='text' placeholder='Address of Nursery' {...register('address')}/>
+    //     <p>{errors.address?.message}</p>
+    //     <input type="email" placeholder='Enter Contact-Email of Nursery' {...register("email")}/>
+    //     <p>{errors.email?.message}</p>
+    //     <input type="text" placeholder='Enter Contact-Number of Nursery' {...register("phone")}/>
+    //     <p>{errors.phone?.message}</p>
+    //     <input type='time' placeholder='Opening Time' {...register("openTime")}/>
+    //     <p>{errors.openTime?.message}</p>
+    //     <input type='time' placeholder='Closing Time' {...register("closeTime")}/>
+    //     <p>{errors.closeTime?.message}</p>
+    //     <input type="text" placeholder='Enter your Off-Day' {...register("offDay")}/>
+    //     <p>{errors.offDay?.message}</p>
+    //     <input type="submit"/>
+    // </form>
+    <section id="nursery_page">
+    <section id="nur_back">
+        <div id="nur_form_cnt">
+           <div id="nur_form_back">
+               <h2>Register Nursery</h2>
+               <form action="" id="nur_form"onSubmit={handleSubmit(onSubmit)}>
+                   <div>
+                       <label for="nur_name">Name of Nursery</label><br/>
+                       <input type="text" id="nur_name"{...register("name")}/>
+                       <p>{errors.name?.message}</p>
+                   </div>
+                   <div>
+                       <label for="nur_addr">Address of Nursey</label><br/>
+                       <input type="text" id="nur_addr"{...register('address')}/>
+                       <p>{errors.address?.message}</p>
+                   </div>
+                   <div>
+                       <label for="nur_email">Contact Email</label><br/>
+                       <input type="email" id="nur_email"{...register("email")}/>
+                       <p>{errors.email?.message}</p>
+                   </div>
+                   <div>
+                       <label for="nur_phone">Contact Number</label><br/>
+                       <input type="text" id="nur_phone"{...register("phone")}/>
+                       <p>{errors.phone?.message}</p>
+                   </div>
+                   <div id="nur_times">
+                       <div>
+                           <label for="nur_in_time">Opening Time</label>
+                           <input type="time" id="nur_in_time"{...register("openTime")}/>
+                           <p>{errors.openTime?.message}</p>
+                       </div>
+                       <div>
+                           <label for="nur_out_time">Closing Time</label>
+                           <input type="time" id="nur_out_time" {...register("closeTime")}/>
+                           <p>{errors.closeTime?.message}</p>
+                       </div>
+                   </div>
+                   <div>
+                       <label for="nur_off_day">Enter Off Day</label><br/>
+                       <input type="text" id="nur_off_day"{...register("offDay")}/>
+                       <p>{errors.offDay?.message}</p>
+                   </div>
+                   <div><button>Submit</button></div>
+               </form>
+                            </div>
+           </div>
+    </section>
+</section>
   )
 }
 
