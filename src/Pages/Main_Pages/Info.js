cc00bfa5de49f12670be6d4462a6c499b27d5c59
Samarth <samarthmali1100@gmail.com>
2023-05-14 14:45:48 +0530

    Weather Lottie Added

--- src/Pages/Main_Pages/Info.js
+++ src/Pages/Main_Pages/Info.js
@@ -1,13 +1,15 @@
 import React from 'react'
 // import BasicModal from '../../Component/Modal'
 import Weather from '../../Component/Weather'
-// import Lottie from '../../Component/Lottie'
-// import weather from '../../static/weather-lottie.json' 
+import Lottie from '../../Component/Lottie'
+import weather from '../../static/weather-lottie.json' 
 
 const Info = () => {
   return (
     <div className='info'>
-      {/* <Lottie data={weather} height={`300px`} width={`300px`} className='weather-lottie'/> */}
+      <div className='weather-lottie'>
+        <Lottie data={weather} height={`300px`} width={`300px`}/>
+      </div>
       <Weather />
     </div>
   )
