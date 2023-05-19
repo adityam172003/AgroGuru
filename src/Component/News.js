760e49f286eaedfafd0382b22ccec852d833d916
Samarth <samarthmali1100@gmail.com>
2023-05-19 21:32:33 +0530

    News API Working

--- src/Component/News.js
+++ src/Component/News.js
@@ -1,45 +1,38 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import Features from './Features';
 import '../Styles/Features.css';
-import { MainContext } from '../context/agroguru_context';
-import Spinner from './Spinner';
 
 
 const News = () => {
-    // const { spin,setSpin } = useContext(MainContext);
-    var flag = true;
-    const [data, setData] = useState({})
-    const [url, setUrl] = useState(`https://newsapi.org/v2/everything?q=agriculture&language=en&pageSize=9&apiKey=15cd77928d8440b988888b289a0c2cf4`);
+    const [data, setData] = useState({articles : [{
+        title : 'News Title',
+        description : 'News Description'
+    }]})
+    
+    const url = `https://newsapi.org/v2/everything?q=agriculture&language=en&pageSize=9&apiKey=15cd77928d8440b988888b289a0c2cf4`;
 
     const getNews = async() => {
         const res = await axios.get(url);
-        flag = false;
         setData(res.data);
         console.log(res.data);
-        flag = false;
     }
 
     useEffect(() => {
-        // axios.get(url).then((response) => {
-        //   setData(response.data)
-        // console.log(response.data)
-        // setSpin(false);
         getNews();
-        flag = false;
     }, [])
     
   
     return (
     <div className='news'>
-        {/* { 
+        { 
             data.articles.map((ob) => {
                 return(
                     <Features title={ob.title} body={ob.description} key={ob.title}/>
                 )
             })
         
-        } */}
+        }
         
     </div>
   )
